7c934b5bdef3c46bf4d7bf2be55f32d91d345801
Hugo <hugo.s.conceicao@gmail.com>
2020-03-21 12:52:46 +0000

    changed portugal source

--- covid.py
+++ covid.py
@@ -1,12 +1,27 @@
 import requests, data
 
+
+
+def get_country_data(country):
+    url = "https://covid19.mathdro.id/api/"
+    request = requests.get(f"{url}/countries/{country}")
+    return request.json()
+
+def get_pt_data(ignore):
+    url = "https://services.arcgis.com/CCZiGSEQbAxxFVh3/arcgis/rest/services/COVID19Portugal_UltimoRel/FeatureServer/0/query?f=json&where=1=1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outSR=102100&resultOffset=0&resultRecordCount=50&cacheHint=true"
+    request = requests.get(url)
+    result = request.json()
+    data = result.get('features')[0].get('attributes')
+    return { "confirmed" : {"value" : data.get('casosconfirmados') } , "deaths" : { "value" : data.get('nrobitos')}}
+
+country_data = {'PT' : get_pt_data}
+
 class Covid(object):
     def __init__(self):
-        self.url = "https://covid19.mathdro.id/api/"
-
-    def get_country_data(self, country):
-        request = requests.get(f"{self.url}/countries/{country}")
-        return request.json()
+        self.general = "https://covid19.mathdro.id/api/"
+        
+    def get_country_data(self, country):     
+        return country_data.get(country, get_country_data)(country)
 
     def get_country_confirmed(self, country):
         info = self.get_country_data(country)
@@ -22,3 +37,6 @@ if __name__ == "__main__":
     covid = Covid()
     print(covid.get_country_confirmed('PT'))
     print(covid.get_country_deaths('PT'))
+
+    print(covid.get_country_confirmed('IT'))
+    print(covid.get_country_deaths('IT'))
