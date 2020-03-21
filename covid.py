9dc52bc331b93816d7912203b792c25b809997f5
Hugo <hugo.s.conceicao@gmail.com>
2020-03-21 17:16:18 +0000

    changed covif generic source

--- covid.py
+++ covid.py
@@ -5,15 +5,24 @@ class Source(object):
         self.countries = {'PT' : self.get_pt_data}
 
     def get_country_data(self, country):
-        url = "https://covid19.mathdro.id/api/"
-        request = requests.get(f"{url}/countries/{country}")
-        return request.json()
- 
+        url = f"https://corona.lmao.ninja/countries/{data.countries.get(country)}"
+        request = requests.get(url)
+        response = request.json()
+        return {
+                "confirmed" : response.get('cases') , 
+                "deaths" : response.get('deaths'), 
+                "recovered" : response.get('recovered')
+                }
+
     def get_pt_data(self, ignore):
         url = "https://services.arcgis.com/CCZiGSEQbAxxFVh3/arcgis/rest/services/COVID19Portugal_UltimoRel/FeatureServer/0/query?f=json&where=1=1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outSR=102100&resultOffset=0&resultRecordCount=50&cacheHint=true"
         request = requests.get(url)
-        data = request.json().get('features')[0].get('attributes')
-        return { "confirmed" : {"value" : data.get('casosconfirmados') } , "deaths" : { "value" : data.get('nrobitos')}}
+        response = request.json().get('features')[0].get('attributes')
+        return {    
+                "confirmed" : response.get('casosconfirmados'), 
+                "deaths" : response.get('nrobitos'), 
+                "recovered" : response.get('recuperados')
+                }
 
 class Covid(object):
     def __init__(self):
@@ -28,12 +37,12 @@ class Covid(object):
 
     def get_country_confirmed(self, country):
         info = self.get_country_data(country)
-        infected = f"O número total de infectados em {data.countries.get(country).capitalize()} é de {info.get('confirmed').get('value')} pessoas. 😷"
+        infected = f"O número total de infectados em {data.countries.get(country).capitalize()} é de {info.get('confirmed')} pessoas. 😷"
         return infected
 
     def get_country_deaths(self, country):
         info = self.get_country_data(country)
-        deaths = f"O número total de mortos em {data.countries.get(country).capitalize()} é de {info.get('deaths').get('value')} pessoas. 💀"
+        deaths = f"O número total de mortos em {data.countries.get(country).capitalize()} é de {info.get('deaths')} pessoas. 💀"
         return deaths
 
 if __name__ == "__main__":
